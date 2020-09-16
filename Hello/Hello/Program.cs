a90f7a49e048eceb3c24ed006297ca2f24fbeff2
Aaron Hales <aaron.hales@tooeleschools.org>
2020-09-16 13:29:04 -0600

    ch 2.3, 2.4, identify yourself

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/* 
+Aaron Hales
+Hello World Example
+CH 2.3
+9/16/20
+*/
+
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myFirstVariable; // declaration statement
+
+            myFirstVariable = "Welcome"; // assignment statment
+
+            string Var2 = "something else";
+
+            Console.WriteLine(myFirstVariable); // This is a console print statment.
+            Console.WriteLine("Press any key to LEAVE"); // Lets the user know how to exit.
+            Console.ReadKey(); // Read key presses.
+            Console.WriteLine("no"); // This
+            Console.ReadKey(); // Is
+            Console.WriteLine("You can never leave"); // The
+            Console.ReadKey(); // Way.
+        }
+    }
+}
---
+++ Identify Yourself/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+Aaron Hales
+Identify Yourself
+CH 2.3
+9/16/20
+*/
+
+
+namespace Identify_Yourself
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aaron Hales"); // print my name
+            Console.WriteLine("Period 1 CTE Internship"); // print first period
+            Console.WriteLine("Period 2 CTE Internship"); // print second period
+            Console.WriteLine("Period 3 Release to parent"); // print third period
+            Console.WriteLine("Period 4 Seminary"); // print fourth period
+            Console.WriteLine("Period 5 Entrepreneurship"); // print fifth period
+            Console.WriteLine("Period 6 Java/Mobile App"); // print sixth period
+            Console.WriteLine("Period 7 Java/Mobile App"); // print seventh period
+            Console.WriteLine("Period 8 English 1010"); // print eighth period
+            Console.WriteLine("Period 9 Game Dev/ C#"); // print ninth period
+            Console.WriteLine("Period 10 Game Dev/ C#"); // print tenth period
+            Console.ReadKey(); // waits for key press
+        }
+    }
+}
