b06743c1af072bb927447b5981b8687ed6f7ece9
AaronHales <aaronmhales@gmail.com>
2020-09-18 13:26:07 -0600

    home pc setup

---
+++ HomePC/HomePC/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomePC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is done on my home pc"); //writes a line of code
+            Console.WriteLine("press una key to continue");
+            Console.ReadKey(); //waits for users to press a key
+        }
+    }
+}
