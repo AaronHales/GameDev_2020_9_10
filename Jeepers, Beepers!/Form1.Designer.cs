25f81c6dbd85bea0d6b118cd14f8b73ff629481e
AaronHales <aaronmhales@gmail.com>
2020-10-12 15:27:34 -0600

    jeepers beepers

---
+++ Jeepers, Beepers!/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Jeepers__Beepers_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.num_beep_lbl = new System.Windows.Forms.Label();
+            this.beeps_txt = new System.Windows.Forms.TextBox();
+            this.for_bttn = new System.Windows.Forms.Button();
+            this.while_bttn = new System.Windows.Forms.Button();
+            this.do_bttn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // num_beep_lbl
+            // 
+            this.num_beep_lbl.AutoSize = true;
+            this.num_beep_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num_beep_lbl.Location = new System.Drawing.Point(18, 38);
+            this.num_beep_lbl.Name = "num_beep_lbl";
+            this.num_beep_lbl.Size = new System.Drawing.Size(137, 20);
+            this.num_beep_lbl.TabIndex = 0;
+            this.num_beep_lbl.Text = "Number of Beeps:";
+            // 
+            // beeps_txt
+            // 
+            this.beeps_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.beeps_txt.Location = new System.Drawing.Point(161, 36);
+            this.beeps_txt.MaxLength = 32678;
+            this.beeps_txt.Name = "beeps_txt";
+            this.beeps_txt.Size = new System.Drawing.Size(45, 24);
+            this.beeps_txt.TabIndex = 1;
+            // 
+            // for_bttn
+            // 
+            this.for_bttn.AutoSize = true;
+            this.for_bttn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.for_bttn.Location = new System.Drawing.Point(47, 66);
+            this.for_bttn.Name = "for_bttn";
+            this.for_bttn.Size = new System.Drawing.Size(140, 28);
+            this.for_bttn.TabIndex = 2;
+            this.for_bttn.Text = "For Loop Button";
+            this.for_bttn.UseVisualStyleBackColor = true;
+            this.for_bttn.Click += new System.EventHandler(this.for_bttn_Click);
+            // 
+            // while_bttn
+            // 
+            this.while_bttn.AutoSize = true;
+            this.while_bttn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.while_bttn.Location = new System.Drawing.Point(47, 100);
+            this.while_bttn.Name = "while_bttn";
+            this.while_bttn.Size = new System.Drawing.Size(140, 28);
+            this.while_bttn.TabIndex = 3;
+            this.while_bttn.Text = "While Loop Button";
+            this.while_bttn.UseVisualStyleBackColor = true;
+            this.while_bttn.Click += new System.EventHandler(this.while_bttn_Click);
+            // 
+            // do_bttn
+            // 
+            this.do_bttn.AutoSize = true;
+            this.do_bttn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.do_bttn.Location = new System.Drawing.Point(47, 134);
+            this.do_bttn.Name = "do_bttn";
+            this.do_bttn.Size = new System.Drawing.Size(140, 28);
+            this.do_bttn.TabIndex = 4;
+            this.do_bttn.Text = "Do Loop Button";
+            this.do_bttn.UseVisualStyleBackColor = true;
+            this.do_bttn.Click += new System.EventHandler(this.do_bttn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(230, 225);
+            this.Controls.Add(this.do_bttn);
+            this.Controls.Add(this.while_bttn);
+            this.Controls.Add(this.for_bttn);
+            this.Controls.Add(this.beeps_txt);
+            this.Controls.Add(this.num_beep_lbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label num_beep_lbl;
+        private System.Windows.Forms.TextBox beeps_txt;
+        private System.Windows.Forms.Button for_bttn;
+        private System.Windows.Forms.Button while_bttn;
+        private System.Windows.Forms.Button do_bttn;
+    }
+}
+
---
+++ Jeepers, Beepers!/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jeepers__Beepers_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void for_bttn_Click(object sender, EventArgs e)
+        {
+            // Declare an int variable and set  it equal to the value in the 
+            // NumBeepsText textbox. Use the  int.Parse() method to translate
+            // the text to a number.
+            int num_beeps = int.Parse(beeps_txt.Text);
+
+            // validate user input to ensure the number is greater than zero
+            if (num_beeps > 0)
+            {
+                // Create a for loop that will  start at 0 and continue while  
+                // 'i' is less than the number of time  the user wants to beep.
+                for (int i = 0; i < num_beeps; i++)
+                {
+                    // Make the computer beep!
+                    Console.Beep();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a positive number");
+            }
+        }
+
+        private void while_bttn_Click(object sender, EventArgs e)
+        {
+            // Create a variable to count the  number of times we have beeped
+            int count = 0;
+
+            // Declare an int variable and set  it equal to the value in the 
+            // NumBeepsText textbox. Use the  int.Parse() method to translate
+            // the text to a number.
+            int num_beeps = int.Parse(beeps_txt.Text);
+
+            // validate user input to ensure the number is greater than zero
+            if (num_beeps > 0)
+            {
+                // Create  while loop that will  loop while  number of times we 
+                // have beeped is less than  number  that  user wants us to beep
+                while (count < num_beeps)
+                {
+                    // Make the computer beep!
+                    Console.Beep();
+                    // increment our beep counter -  VERY IMPORTANT!!
+                    count++;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a positive number");
+            }
+        }
+
+        private void do_bttn_Click(object sender, EventArgs e)
+        {
+            // Create a variable to count the  number of times we have beeped
+            int count = 0;
+
+            // Declare an int variable and set  it equal to the value in the 
+            // NumBeepsText textbox. Use the  int.Parse() method to translate
+            // the text to a number.
+            int num_beeps = int.Parse(beeps_txt.Text);
+
+            // validate user input to ensure the number is greater than zero
+            if (num_beeps > 0)
+            {
+                // Create a do loop that will loop  while  the number of times 
+                // we have beeped is less than the number  the user entered
+                do
+                {
+                    // Make the computer beep!
+                    Console.Beep();
+                    // increment our beep counter -  VERY IMPORTANT!!
+                    count++;
+                }
+                while (count < num_beeps);
+            }
+            else
+            {
+                MessageBox.Show("Please enter a positive number");
+            }
+        }
+    }
+}
