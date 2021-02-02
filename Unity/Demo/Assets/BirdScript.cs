b347d2c24be8c7a94675c9a254a3a80a9952a623
Aaron Hales <aaron.hales@tooeleschools.org>
2021-02-02 14:13:22 -0700

    chapter 3 lessons, need to complete chapter 3 activity

---
+++ Unity/Demo/Assets/BirdScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    /*
+     * Game Dev
+     * Ch 3 lesson 3
+     * Aaron Hales
+     * 2/2/21
+     */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I'm Alive");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("I'm updating...");
+    }
+}
