64f64278d67c18b38d32722909347e4aa1b76062
Aaron Hales <aaron.hales@tooeleschools.org>
2021-02-04 14:14:51 -0700

    chapter 3 activity

---
+++ Unity/Flock/Assets/BirdScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Gaming Development
+ * Chapter 3 Activity
+ * Aaron Hales
+ * 2/4/21
+ */
+
+public class BirdScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(this.gameObject.name + " Reporting for duty!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
