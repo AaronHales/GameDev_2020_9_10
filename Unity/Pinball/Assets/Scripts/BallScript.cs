2bc671f9b20018778df31fba35e9b87b6376ee1f
AaronHales <aaronmhales@gmail.com>
2021-02-18 13:25:04 -0700

    pinball activity for chapter 5

---
+++ Unity/Pinball/Assets/Scripts/BallScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Game Development
+ * Chapter 5 Activity: Simple Pinball
+ * Aaron Hales
+ * 2/18/21
+ */
+
+public class BallScript : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D otherObject)
+    {
+        Debug.Log("Game Over!");
+    }
+
+    void OnCollisionEnter2D(Collision2D otherObject) 
+    {
+      // print a message showing  physics-based collisions
+      Debug.Log("Collision with " + otherObject.gameObject.name);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity/Pinball/Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Game Development
+ * Chapter 5 Activity: Simple Pinball
+ * Aaron Hales
+ * 2/18/21
+ */
+
+public class PaddleScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal"), 0, 0);
+    }
+}
