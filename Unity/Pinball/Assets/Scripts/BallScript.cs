1d659ef922a554632f9f372355717747088dcf40
AaronHales <aaronmhales@gmail.com>
2021-02-24 13:13:21 -0700

    finished chapter 6 lesson 1 files, and completed chapter 6 activity, Pinball

--- Unity/Pinball/Assets/Scripts/BallScript.cs
+++ Unity/Pinball/Assets/Scripts/BallScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /*
  * Game Development
@@ -11,26 +12,34 @@ using UnityEngine;
 
 public class BallScript : MonoBehaviour
 {
+  int score = 0;
+  public Text scoreText;
+  public Text gameOverText;
+
     void OnTriggerEnter2D(Collider2D otherObject)
     {
-        Debug.Log("Game Over!");
+      Debug.Log("Game Over!");
+      gameOverText.text = "Game Over!";
     }
 
     void OnCollisionEnter2D(Collision2D otherObject) 
     {
       // print a message showing  physics-based collisions
       Debug.Log("Collision with " + otherObject.gameObject.name);
+      score++;
+      scoreText.text = "Score: " + score;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+      scoreText.text = "Score: " + score;
+      gameOverText.text = "";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
---
+++ Unity/SharkTag/Assets/Scripts/SharkScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SharkScript : MonoBehaviour {
+
+	// declare the score variable and set to 0
+	int score = 0;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Debug.Log("Score = " + score);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// move the shark according to the inputs
+		transform.Translate (Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+	}
+
+	void OnCollisionEnter2D (Collision2D otherObject) 
+	{
+		score++;
+		Debug.Log("Score = " + score);
+	}
+}
